fd035d1fe8154db617e0a7742fd3eaad662352eb
maskas <mangirdas@impresspages.org>
2014-08-29 23:57:38 +0300

    fix options

--- Filter.php
+++ Filter.php
@@ -8,13 +8,13 @@ class Filter
     public static function ipSendEmailPHPMailerObject(\PHPMailer $mail)
     {
         $mail->IsSMTP(); // telling the class to use SMTP
-        $mail->Host = ipGetOption('SMTConfig.server', 'smtp.gmail.com'); // SMTP server
+        $mail->Host = ipGetOption('SMTPConfig.server', 'smtp.gmail.com'); // SMTP server
         //$mail->SMTPDebug = 2; // enables SMTP debug information (for testing)
-        $mail->SMTPAuth = ipGetOption('SMTConfig.smtpAuth', true); // enable SMTP authentication
-        $mail->Port = ipGetOption('SMTConfig.port', 465); // set the SMTP port for the GMAIL server
-        $mail->Username = ipGetOption('SMTConfig.username'); // SMTP account username
-        $mail->Password = ipGetOption('SMTConfig.password');
-        $mail->SMTPSecure = strtolower(ipGetOption('SMTConfig.secure', 'ssl'));
+        $mail->SMTPAuth = ipGetOption('SMTPConfig.smtpAuth', true); // enable SMTP authentication
+        $mail->Port = ipGetOption('SMTPConfig.port', 465); // set the SMTP port for the GMAIL server
+        $mail->Username = ipGetOption('SMTPConfig.username'); // SMTP account username
+        $mail->Password = ipGetOption('SMTPConfig.password');
+        $mail->SMTPSecure = strtolower(ipGetOption('SMTPConfig/.secure', 'ssl'));
         return $mail;
     }
 }
